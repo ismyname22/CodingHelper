2f7634e370810ac7fed22a1109dc4feb2586704b
ismyname22 <133056901+ismyname22@users.noreply.github.com>
2026-02-04 12:06:37 +0100

    Add deterministic CAD pipeline

---
+++ pipeline/__init__.py
@@ -0,0 +1,16 @@
+from pipeline.pipeline import (
+    conversation_to_spec,
+    run_pipeline,
+    spec_to_geometry,
+    validate_spec,
+)
+from pipeline.spec import Spec
+
+__all__ = [
+    "Spec",
+    "conversation_to_spec",
+    "run_pipeline",
+    "spec_to_geometry",
+    "validate_spec",
+]
+
